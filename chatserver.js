547a42cc742826504e12179f4dbb79e304091b4c
DednaKRaZe <ilyakorgin@gmail.com>
2025-03-06 12:22:49 +0300

    pr4

--- chatserver.js
+++ chatserver.js
@@ -6,15 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Обработчик подключения WebSocket
+
 wss.on('connection', (ws) => {
     console.log('Новое соединение установлено');
 
-    // Обработчик сообщений от клиента
+
     ws.on('message', (message) => {
         console.log(`Получено сообщение: ${message}`);
 
-        // Отправляем сообщение всем подключенным клиентам
         wss.clients.forEach((client) => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message);
@@ -22,16 +21,15 @@ wss.on('connection', (ws) => {
         });
     });
 
-    // Обработчик закрытия соединения
     ws.on('close', () => {
         console.log('Соединение закрыто');
     });
 });
 
-// Статический сервер для обслуживания HTML-файла
+
 app.use(express.static('public'));
 
-// Запуск сервера
+
 server.listen(8081, () => {
     console.log('Сервер запущен на http://localhost:8081');
 });
